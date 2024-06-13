2e4cdb7d1b60567b124271c2d64d0d8828aedc98
Kushal Lalwani <kushallalwani.kl@gmail.com>
2024-06-13 22:35:26 +0530

    Improved Search Design

--- src/Components/Header/Search.jsx
+++ src/Components/Header/Search.jsx
@@ -1,14 +1,18 @@
 import { useForm } from "react-hook-form";
+import { FaSearch } from "react-icons/fa";
 
 const Search = () => {
   const { register, handleSubmit } = useForm();
 
   return (
-    <div >
+    <div className="bg-[#202020]">
       <form onSubmit={handleSubmit((data) =>{
         console.log(data);
       })}>
-        <input {...register("Search")} placeholder="Search Videos" className="w-full px-3 py-2 outline-none border-2 border-slate-700 bg-[#191919]"/>
+        <div className="flex rounded-lg ">
+          <input {...register("Search")} placeholder="Search Videos" className="w-11/12 rounded-r-none rounded-lg outline-none border border-slate-700  px-3 py-1.5 bg-[#202020]"/>
+          <div className="w-1/12 flex rounded-lg rounded-l-none border-l-2 border-red-700 bg-red-700"><button className="m-auto justify-center items-center pr-0.5"><FaSearch/></button></div>
+        </div> 
       </form>
     </div>
   );
